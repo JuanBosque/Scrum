05b985552f23aba9130fd8e755dfac3de980dc70
marcos <marcosromero6411@gmail.com>
2021-04-14 13:15:02 +0200

    Ejer7

---
+++ Form7.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Scrum
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMultilicar = new System.Windows.Forms.Button();
+            this.listaVectores = new System.Windows.Forms.ListBox();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMultilicar
+            // 
+            this.btnMultilicar.Location = new System.Drawing.Point(39, 282);
+            this.btnMultilicar.Name = "btnMultilicar";
+            this.btnMultilicar.Size = new System.Drawing.Size(254, 33);
+            this.btnMultilicar.TabIndex = 0;
+            this.btnMultilicar.Text = "multiplicar vector";
+            this.btnMultilicar.UseVisualStyleBackColor = true;
+            this.btnMultilicar.Click += new System.EventHandler(this.btnMultilicar_Click);
+            // 
+            // listaVectores
+            // 
+            this.listaVectores.FormattingEnabled = true;
+            this.listaVectores.ItemHeight = 16;
+            this.listaVectores.Location = new System.Drawing.Point(39, 31);
+            this.listaVectores.Name = "listaVectores";
+            this.listaVectores.Size = new System.Drawing.Size(173, 196);
+            this.listaVectores.TabIndex = 1;
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Location = new System.Drawing.Point(415, 41);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(149, 22);
+            this.txtSalida.TabIndex = 2;
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.listaVectores);
+            this.Controls.Add(this.btnMultilicar);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMultilicar;
+        private System.Windows.Forms.ListBox listaVectores;
+        private System.Windows.Forms.TextBox txtSalida;
+    }
+}
---
+++ Form7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrum
+{
+    public partial class Form7 : Form
+    {
+        public Form7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultilicar_Click(object sender, EventArgs e)
+        {
+            int numMulti = 0;
+            var rand = new Random();
+            int[] miArray = new int[10];
+
+            for (int i = 0; i < miArray.Length; i++)
+            {
+                miArray[i] = rand.Next(0, 11);
+                listaVectores.Items.Add(miArray[i]);
+            }
+            numMulti = miArray[0];
+            for (int i = 1; i < miArray.Length; i++)
+            {
+                numMulti = numMulti * miArray[i];
+                listaVectores.Items.Add(miArray[i]);
+            }
+            txtSalida.Text = numMulti.ToString();
+        }
+    }
+}
