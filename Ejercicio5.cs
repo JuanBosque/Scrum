dd5e80e6d410e2b099434b8bdbfd1c7f74cbc63a
estelagotapuertolas@fp.oceanoatlantico.org <estelagotapuertolas@fp.oceanoatlantico.org>
2021-04-14 11:05:55 +0200

    Ejercicio5

---
+++ Ejercicio5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrum
+{
+    public partial class Ejercicio5 : Form
+    {
+        public Ejercicio5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculo_Click(object sender, EventArgs e)
+        {
+            char[] caracteres;
+            int num = 0;
+            var numero = txtEntrada.Text;
+            for (int i = 0; i < numero.Length; i++)
+            {
+                num = num + numero[i];
+            }
+            txtResultado.Text = num.ToString();
+        }
+    }
+}
