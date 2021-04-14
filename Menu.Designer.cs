4c33d24aa33cb587a0cac103bbb70c2dc1e871b9
dam1d01javidu <javidu14@gmail.com>
2021-04-14 11:12:12 +0200

    MEnu1

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -150,7 +150,7 @@ namespace Scrum
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(150, 189);
+            this.label1.Location = new System.Drawing.Point(160, 189);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(506, 39);
             this.label1.TabIndex = 11;
